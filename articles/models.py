f9054d0189704153c52ffc47a1b83404d1f15e88
Andrew Farrell <afarrell@mit.edu>
2013-03-31 14:49:42 -0400

    modularize translation.

--- articles/models.py
+++ articles/models.py
@@ -1,10 +1,26 @@
+from itertools import chain
+from translate import word_tokenize
+from translate import sent_tokenize #wordpunct_tokenize
+from translate import SnowballStemmer
+from translate import translate_word
+
+
+import apiclient.discovery
+
 from django.db import models
 from django.db.models import permalink
 
-from itertools import chain
-from nltk import word_tokenize
-from nltk import sent_tokenize #wordpunct_tokenize
-from nltk import SnowballStemmer
+
+from settings import GOOGLE_TRANSLATE_API_KEY
+
+
+service = apiclient.discovery.build('translate', 'v2', developerKey=GOOGLE_TRANSLATE_API_KEY)
+
+def ifthere(word):
+  if word is None:
+    return ''
+  else:
+    return ' %s' % word
 
 class Language(models.Model):
   code = models.CharField(max_length = 8, unique = True)
@@ -24,6 +40,63 @@ class Article(models.Model):
   source_url = models.CharField(max_length = 2048, unique = True)
   source_lang = models.ForeignKey('articles.Language')
 
+  def scentences(self):
+    return sent_tokenize(self.body)
+
+  def words(self):
+    #TODO inefficient.
+    return [i for i in chain.from_iterable([word_tokenize(t) for t in self.scentences()])]
+
+  def parsed_article(self, target_langauge = 'en', force = False):
+    raise NotImplementedError
+    if target_langauge != 'en':
+      raise NotImplementedError("Only English translation for now.")
+    target_langauge = Language.objects.get(code = target_langauge) #TODO: make this resilient.
+    trans = Translation.objects.filter(original_article = self, target_langauge = target_langauge)
+    if force:
+      trans.delete()
+    if trans.exists():
+      return trans.get()
+    else:
+      return self.translate(target_langauge)
+
+
+  def translate(self, target_langauge = 'en'):
+    if type(target_langauge) is str:
+      target_langauge = Language.objects.get(code = target_langauge)
+    if target_langauge.code != 'en':
+      raise NotImplementedError("Only English translation for now.")
+    # todo:
+    target_langauge = Language.objects.get(code = target_langauge.code)
+    tokens = self.words() #TODO: make this use iterators.
+    pass
+#    stemmer = SnowballStemmer(self.source_lang.name.lower())
+#    length = len(tokens)
+#    i = 0
+#    old_index = None
+#    translation = Translation.objects.create(original_article = self, target_langauge = target_langauge)
+#
+#    while i < length:
+#      current_text = tokens[i]
+#      current_stem = stemmer.stem(current_text)
+#      current_word = Word.objects.filter(native_text = current_text, native_language = self.source_lang)
+#      if current_word.count() == 0:
+#        current_word = Word.objects.create(native_text = current_text, native_language = self.source_lang, native_stem = current_stem, english_text = current_stem)
+#      else:
+#        current_word = current_word.get()
+#
+#      if i == 0:
+#        pa = Translation.objects.create(original_article = self, target_langauge = target_langauge, first_word = current_word)
+#      #if bool(old_index): 
+#      #  current_index = WordInArticle.objects.create(word_id = current_word.id, article_id = pa.id, prev_id = old_index.id)
+#      #else:
+#      #  current_index = WordInArticle.objects.create(word = current_word, article = pa)
+#
+#      #old_index = current_index
+#      i += 1
+#
+#    pa.save()
+#    return pa
 
   @permalink
   def get_absolute_url(self):
@@ -48,7 +121,7 @@ class Translation(models.Model):
   An article prepared to be read by someone in a foreign language.
   """
   original_article = models.ForeignKey(Article)
-  dest_lang = models.ForeignKey(Language) #TODO: add en as default.
+  target_langauge = models.ForeignKey(Language)
 
   @permalink
   def get_absolute_url(self):
@@ -73,11 +146,15 @@ class Word(models.Model):
   def __unicode__(self):
     return "%s" % self.native_text
 
-def ifthere(word):
-  if word is None:
-    return ''
-  else:
-    return ' %s' % word
+  def translate_to(self, target_language = 'en'):
+    if type(target_language) is str:
+      target_language = Language.objects.get(code = target_language)
+    target_text = translate_word(self.native_text, self.native_language.code, target_language.code)
+    target_stemmer = SnowballStemmer(target_language.name.lower())
+    target_stem = target_stem = Stem.objects.create(native_language = target_language, native_text = target_stemmer.stem(target_text))
+    result = Word.objects.get_or_create(native_text = target_text, native_stem = target_stem, native_language = target_language)[0]
+    #TranslatedPhrase.objects.create(first_target = result, first_native = self)
+    return result
 
 class TranslatedPhrase(models.Model):
   #TODO: constrain that these words are all the same language.
--- articles/translate.py
+++ articles/translate.py
@@ -6,40 +6,26 @@ import apiclient.discovery
 
 from settings import GOOGLE_TRANSLATE_API_KEY
 
-from models import Word
-from models import Stem
-from models import Language
-from models import TranslatedPhrase
-
-
 service = apiclient.discovery.build('translate', 'v2', developerKey=GOOGLE_TRANSLATE_API_KEY)
 
-def translateword(word, native_language = None, target_language = 'en'):
-  if type(target_language) is str:
-    target_language = Language.objects.get(code = target_language)
-  if type(word) is str and native_language is not None:
-    if type(native_language) is str:
-      native_language = Language.objects.get(code = native_language)
-    native_stemmer = SnowballStemmer(native_language.name.lower())
-    stem = Stem.objects.create(native_text = native_stemmer.stem(word), native_language = native_language)
-    word = Word.objects.get_or_create(native_stem = stem, native_text = word, native_language = native_language)[0]
-  target_text = service.translations().list(source = word.native_language.code,
-                              target = target_language.code,
-                              q = [word.native_text,]).execute()
-  target_stemmer = SnowballStemmer(target_language.name.lower())
+def translate_word(native_text, native_language, target_language = 'en'):
+  if type(target_language) is not str:
+    target_language = target_language.code
+  if type(native_language) is not str:
+    native_language = native_language.code
+  target_text = service.translations().list(source = native_language,
+                              target = target_language,
+                              q = [native_text,]).execute()
   target_text = target_text['translations'][0]['translatedText']
-  target_stem = target_stem = Stem.objects.create(native_language = target_language, native_text = target_stemmer.stem(target_text))
-  result = Word.objects.get_or_create(native_text = target_text, native_stem = target_stem, native_language = target_language)[0]
-  translation = TranslatedPhrase.objects.create(first_target = result, first_native = word)
-  return translation
+  return target_text
 
-def translatesentence(sentence, native_language, target_language):
+def translate_sentence(sentence, native_language, target_language = 'en'):
   words = word_tokenize(sentence)
-  translations = [translateword(word, native_language, target_language) for word in words]
-  return ' '.join(translation.resulting_text() for translation in translations)
+  translations = [translate_word(word, native_language, target_language) for word in words]
+  return ' '.join(translation for translation in translations)
 
-def translateparagraph(paragraph, native_language, target_language):
+def translate_paragraph(paragraph, native_language, target_language = 'en'):
   sentences = sent_tokenize(paragraph)
-  translations = [translatesentence(sentence, native_language, target_language) for sentence in sentences]
+  translations = [translate_sentence(sentence, native_language, target_language) for sentence in sentences]
   return ' '.join(translation for translation in translations)
 
