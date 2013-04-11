5a6d84b9ab06024e52478d662c6bcca7bfadde40
Andrew Farrell <afarrell@mit.edu>
2013-04-11 17:22:58 -0400

    make urls work

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,9 +16,10 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url('^account/(?P<path>.*)$', redirect_to, {'url': '/accounts/%(path)s'}),
     url(r'^accounts/', include(learning.urls)),
     url(r'^learning/', include(learning.urls)),
-#    url(r'^(?P<code>[^\.]+)/(?P<slug>[^\.]+)', ('articles.views.view_article')),
-#    url(r'^(?P<code>[^\.]+)/', ('articles.views.view_language')),
+    url(r'^(?P<code>[^\.]+)/(?P<slug>[^\.]+)', ('articles.views.view_article')),
+    url(r'^(?P<code>[^\.]+)/', ('articles.views.view_language')),
 #    url(r'^', ('articles.views.index')),
 )
