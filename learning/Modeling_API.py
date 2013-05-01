6b3336b00b0aecb3fdd7b0446daddabe0a7fce62
brian <none>
2013-05-01 12:48:43 -0400

    Updated pretest.html file.

--- learning/Modeling_API.py
+++ learning/Modeling_API.py
@@ -29,14 +29,14 @@ If after 3 reads, if no clicks, then set word to known.
 Anytime a word click happens, it resets to zero.
 """
 
-def populateModel(user, level):
+def populateModel(user_id, level):
 """
 Initialize the model of the user. 
 All word levels labled 1-6, 1 being the first 1 thousand, etc.
 Mastery_level is just a boolean value.
 
 Keyword arguments:
-  user - the username of type string
+  user_id - the user id of type int
   level - the user level from 1 to 6.
 """
   words = Word.objects.get(difficulty__lte = level)
@@ -53,7 +53,7 @@ def callOnClick(user_id, word_id):
   lookup click. This updates the model.
 
   Keyword arguments:
-    user_id - the pk of username of type int
+    user_id - the primary key (pk) of username of type int
     word_id - the pk of word of type int
   """
   word = Word.objects.get(pk=word_id)
@@ -71,8 +71,8 @@ def callOnArticleUpload(user_id, article_id):
   count of every word.
 
   Keyword arguments:
-    user - the username of type string
-    title - the title of the article of type string
+    user_id - the user id of type int
+    article_id - the pk of the article of type int
   """
   article = Article.objects.get(pk=article_id)
   text = article.words()
@@ -88,35 +88,35 @@ def callOnArticleUpload(user_id, article_id):
 
 
 
-def scoreArticle(user, title):
+def scoreArticle(user_id, article_id):
   """
   Returns ratio of words known to total words.
   Includes duplicate words.
 
   Keyword arguments:
-    user - the username of type string
-    title - the title of the article of type string
+    user_id - the user id of type int
+    article_id - the pk of the article of type int
   """
-  article = Article.objects.get(title = title)
+  article = Article.objects.get(article_id = article_id)
   score = 0.0
-  for w in article.words():
-    if UserWordKnowledge.objects.get(word=w) not None:
+  for word in article.words():
+    if UserWordKnowledge.objects.get(user_id, word=word) not None:
       score += 1
   return score/len(article.words())
 
 
-def rankArticles(user, titles):
+def rankArticles(user_id, article_ids):
   """
-  Takes in a list of article names and returns the ranked order.
+  Takes in a list of article ids and returns the ranked order.
 
   Keyword arguments:
-    user - the username of type string
-    titles - a list of string titles
+    user_id - the username of type string
+    article_ids - a list of int article id's
   """
   ranking = []
-  for title in titles:
-    score = scoreArticle(user, title)
-    ranking.append((score, title))
+  for article_id in article_ids:
+    score = scoreArticle(user_id, article_id)
+    ranking.append((score, article_id))
   ranking.reverse()
   ranking = [title for score, title in ranking]
   return ranking
