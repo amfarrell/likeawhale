3aedce8893d6d26bb5ae62e27cd1eafaaa1593ce
brian <none>
2013-05-01 00:55:59 -0400

    Integrated update files.

--- learning/Modeling_API.py
+++ learning/Modeling_API.py
@@ -47,23 +47,24 @@ Keyword arguments:
 
 
 # Sets word to unknown.
-def callOnClick(user, word):
+def callOnClick(user_id, word_id):
   """
   Function needs to be called on a user word definition
   lookup click. This updates the model.
 
   Keyword arguments:
-    user - the username of type string
-    word - the word of type string
+    user_id - the pk of username of type int
+    word_id - the pk of word of type int
   """
-  vector = UserWordKnowledge.objects.filter(user=user, word=word)
+  word = Word.objects.get(pk=word_id)
+  vector = UserWordKnowledge.objects.get(user_id=user_id, word=word)
   vector.mastery_level = 0
   vector.view_count = 0
   vector.save()
 
 
 # Assumes an Article words() method.
-def callOnArticleUpload(self, user, title):
+def callOnArticleUpload(user_id, article_id):
   """
   Function needs to be called on an article click.
   When an article is displayed, increase the read 
@@ -73,11 +74,11 @@ def callOnArticleUpload(self, user, title):
     user - the username of type string
     title - the title of the article of type string
   """
-  article = Article.objects.filter(title=title)
+  article = Article.objects.get(pk=article_id)
   text = article.words()
 
   for word in text:
-    vector = UserWordKnowledge.objects.filter(user=user, word=word)
+    vector = UserWordKnowledge.objects.filter(user_id=user_id, word=word)
     vector.view_count += 1
     vector.save()
     if vector.view_count >= 3:
